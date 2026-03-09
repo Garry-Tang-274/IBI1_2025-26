ca8ff73711de9c4b7874879de2e5e0d6d585d2ed
root <root@TangZhiOMEN.localdomain>
2026-03-09 17:17:43 +0800

    download and add annotations to mystery_code

---
+++ Practical4/mystery_code.py
@@ -0,0 +1,21 @@
+# What does this piece of code do?
+# Answer:This code generates 11 random integers from 1 to 10, calculates their total sum, and outputs the total.
+
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint # Import the randint function from the random library to generate random integers
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil # Import the ceil function from the math library (seems that it has not been used in the code)
+
+total_rand = 0  # Initialize the variable total_rand to store the total sum of random numbers, with an initial value of 0
+progress=0 # Initialize the variable progress to control the number of loops, with an initial value of 0
+while progress<=10:  # Start the loop when progress is less than or equal to 10 (the loop runs 11 times in total)
+	progress+=1 # Increase progress by 1 in each loop
+	n = randint(1,10) # Generate a random integer between 1 and 10 and store it in the variable n
+	total_rand+=n # Add the randomly generated number n to total_rand in this loop
+
+print(total_rand) # Print the final total sum of the random numbers
+
